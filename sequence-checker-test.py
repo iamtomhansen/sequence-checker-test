02392596d4aaace541a09f53053d8196ed8fc9b9
Thomas Hansen <iamtomhansen@gmail.com>
2021-06-29 23:25:15 -0400

    Add files via upload

--- sequence-checker-test.py
+++ sequence-checker-test.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 24 21:28:07 2021
-
 @author: thomas
 """
 #imports
-import pandas as pd # for future use of dataframes
+# import pandas as pd # for future use of dataframes
 import streamlit as st
-import base64 # for future downloads to csv/xlsx
+# import base64 # for future downloads to csv/xlsx
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio import pairwise2
-from PIL import Image # for future images
+# from PIL import Image # for future images
 
 # Page config
 st.set_page_config(page_title='Sequence data checker', page_icon='🔬', layout='centered', initial_sidebar_state='auto')
@@ -19,7 +18,7 @@ st.set_page_config(page_title='Sequence data checker', page_icon='🔬', layout=
 # Page title
 st.title('Sequence data checker :microscope:')
 st.markdown('''
-            Last edited on June 25 2021 by Thomas Hansen. Contact 21th14 at queensu dot ca
+            Last edited on June 29 2021 by Thomas Hansen. Contact 21th14 at queensu dot ca
             
             For now, this is a personal learning experience, not a productivity tool. Use at your own risk.
             ***
@@ -45,16 +44,53 @@ def trimend(seq,start):
         i=len(seq)
     return i
 
+def seq_filetype(file):
+    filetypes={
+        '.abi':'abi',
+        '.ab1':'abi',
+        '.aln':'clustal',
+        '.fa':'fasta',
+        '.fasta':'fasta',
+        '.fna':'fasta',
+        '.fastq':'fastq',
+        '.fq':'fastq',
+        '.embl':'genbank',
+        '.gb':'genbank',
+        '.gbk':'genbank',
+        '.genbank':'genbank',
+        '.scf':'snapgene'
+    }
+    dotposition = file.name.rfind('.')
+    filetype = filetypes.get(file.name[dotposition:len(file.name)],'unknown')
+    return filetype
+
+def query_aligner(query,seq_alignto):
+    try: # Future: Could support protein queries (and make it force uppercase if I do)
+        if query_type=='Nucleic':
+            query=Seq(query).translate() # translate if it's nucleic
+        score=seq_alignto.count_overlap(query)
+    except:
+        score='?'
+    return score
+
 # Sidebar for uploading
 with st.sidebar.header('Sequencing data goes here'):
-    uploaded_files = st.sidebar.file_uploader("Only .ab1 for now", accept_multiple_files=True)
-    query=st.sidebar.text_input("Query (optional) EXPERIMENTAL")
-
+    uploaded_files = st.sidebar.file_uploader("Only abi, fasta, genbank, and snapgene files work for now.", accept_multiple_files=True)
+    query=st.sidebar.text_input("Query (optional)")
+if query:
+    query_type = st.sidebar.radio('',['Nucleic','Amino'])
+    filter_query = st.sidebar.checkbox('Exclude mismatches',value=False)
+ 
 # Main panel
+
 if st.sidebar.button('Submit'): # On clicking the button
     for file in uploaded_files: # For each file
-        # !!!Determine filetype in the future
-        filetype='abi'
+        # Determine filetype in the future
+        filetype=seq_filetype(file)
+        if filetype=='unknown': # skip the file if it can't handle it
+            st.header("Sorry, couldn't read this file format: "+file.name)
+            st.write('---')
+            continue
         for record in SeqIO.parse(file,filetype): # For each record in the file
             st.header("Filename: "+file.name)
             first_nt = trimstart(record.seq) # Get through the N's at the start
@@ -67,26 +103,20 @@ if st.sidebar.button('Submit'): # On clicking the button
                 # translate the forward strand
                 translated=trimmed[frame_start:].translate()
                 if query: # is query is not empty, will return True
-                    try: # Future: Could support protein queries (and make it force uppercase if I do)
-                        querytranslated=Seq(query).translate()
-                        score=pairwise2.align.localxx(translated,querytranslated,score_only=True) # Simple alignment, match = +1 score
-                    except:
-                        score='?'
-                    st.write("Forward (5'→3') translation in Frame",str(frame_start+1),", Stops:",str(translated.count('*')),", Identities:",str(score))
-                    st.write(translated)
+                    score=query_aligner(query,translated)
+                    if score != 0:
+                        st.write("Forward (5'→3') translation in Frame",str(frame_start+1),", Stops:",str(translated.count('*')),", Matches:",str(score))
+                        st.write(translated)
                 else:
                     st.write("Forward (5'→3') translation in Frame",str(frame_start+1),", Stops:",str(translated.count('*')))
                     st.write(translated)
                 # Now the reverse complement
                 translated=trimmed.reverse_complement()[frame_start:].translate()
                 if query:
-                    try:
-                        querytranslated=Seq(query).translate()
-                        score=pairwise2.align.localxx(translated,querytranslated,score_only=True)
-                    except:
-                        score='?'
-                    st.write("Reverse (3'→5') translation in Frame",str(frame_start+1),", Stops:",str(translated.count('*')),", Identities:",str(score))
-                    st.write(translated)
+                    score=query_aligner(query,translated)
+                    if score != 0:
+                        st.write("Reverse (3'→5') translation in Frame",str(frame_start+1),", Stops:",str(translated.count('*')),", Matches:",str(score))
+                        st.write(translated)
                 else:
                     st.write("Reverse (3'→5') translation in Frame",str(frame_start+1),", Stops:",str(translated.count('*')))
                     st.write(translated)
